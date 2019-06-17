f3db0d9f14532687be727621ff9a5ba3fd1debbb
RicaroPeraltaM <r.peralta.rp70@gmail.com>
2019-06-17 14:20:34 -0500

    cuartCommit

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ process.env.PORT = process.env.PORT || 3000;
 
 
 // ====================================
-// Vencimiento del token
+// Vencimiento del token Jc es puto
 // ====================================
 
 process.env.CADUCIDAD_TOKEN = '24h'
